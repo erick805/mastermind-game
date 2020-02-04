336349e4d555581999b099d6ead91d2e39e0348a
Ricky Lau <rickylaufitness@gmail.com>
2020-02-04 13:23:41 -0500

    create-edit: Game class with constructor, refactor fetchCombinationAndRender

--- mastermind.js
+++ mastermind.js
@@ -1,4 +1,4 @@
-const fetchCombinationAndRender = async () => {
+const fetchCombination = async () => {
   try {
     const res = await fetch(
       `https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new`
@@ -6,10 +6,9 @@ const fetchCombinationAndRender = async () => {
 
     if (res.status === 200) {
       const data = await res.text();
-      const combinations = data.split("\n");
+      const combinations = data.split("\n").filter(c => c.length);
 
-      renderCombinations(combinations);
-      return data.split("\n");
+      return combinations;
     } else if (res.status === 503) {
       console.error(await res.text());
     } else if (res.status === 301) {
@@ -18,6 +17,35 @@ const fetchCombinationAndRender = async () => {
   } catch (err) {
     console.error(err);
   }
+};
+
+class Game {
+  constructor() {
+    this._winningCombination = [];
+  }
+  static init() {
+    return (async function() {
+      const game = new Game();
+      await game.build();
+      return game;
+    })();
+  }
+
+  async build() {
+    const combination = await fetchCombination();
+    this._winningCombination = combination;
+  }
+}
+
+async function newGame() {
+  const game = await Game.init();
+}
+
+newGame();
+
+const fetchCombinationAndRender = async () => {
+  const combinations = await fetchCombination();
+  renderCombinations(combinations);
 };
 
 const renderCombinations = combinations => {
